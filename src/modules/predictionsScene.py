6a00fe4bb488e6e7bb33fff53c88797901624e15
Danny Garcia <dannygarciacs@gmail.com>
2022-12-27 12:06:16 -0500

    Blocked predictions design

--- src/modules/predictionsScene.py
+++ src/modules/predictionsScene.py
@@ -1,3 +1,33 @@
+# Imports
+import pygame as pg
 
+# Initializes predictions scene
 def boot(window, settings):
-    pass
+
+    # Scene variables
+    clock = pg.time.Clock()
+
+    # Main scene loop
+    while True:
+
+        # Updates window
+        window.update()
+        window.fill((0, 0, 0))
+        clock.tick(60)
+
+        # Event handling
+        for event in pg.event.get():
+
+            # Cross is pressed
+            if event.type == pg.QUIT: return False, ""
+
+        pg.draw.rect(window.display, (150, 50, 50), pg.Rect(0, 0, 125, 1080))
+        pg.draw.rect(window.display, (200, 75, 75), pg.Rect(15, 15, 95, 95))
+        pg.draw.rect(window.display, (200, 75, 75), pg.Rect(15, 125, 95, 95))
+        pg.draw.rect(window.display, (200, 75, 75), pg.Rect(15, 235, 95, 95))
+        pg.draw.rect(window.display, (200, 75, 75), pg.Rect(15, 970, 95, 95))
+
+        pg.draw.rect(window.display, (50, 50, 150), pg.Rect(140, 15, 1765, 515))
+
+        pg.draw.rect(window.display, (50, 150, 50), pg.Rect(140, 545, 1765, 520))
+        pg.draw.rect(window.display, (75, 200, 75), pg.Rect(140, 545, 1765, 80))
